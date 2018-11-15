ea8712c23a07c0f0a749e31d74968ed11af984d7
h1tendra <hitendra.http@gmail.com>
2018-11-15 23:52:43 +0530

    send commit desc to hockey app & notify all testers

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
+GIT_COMMIT_DESC=$1
 curl \
 -F "status=2" \
--F "notify=1" \
+-F "notify=2" \
+-F "notes=$GIT_COMMIT_DESC" \
+-F "notes_type=0" \
 -F "ipa=@platforms/android/app/build/outputs/apk/debug/app-debug.apk" \
 -H "X-HockeyAppToken: 2161769b31f54dc3a4dc9bef41b74300" \
 https://rink.hockeyapp.net/api/2/apps/1b5edbbfbdb94a3a8eba43ef6a5f8f09/app_versions/upload
