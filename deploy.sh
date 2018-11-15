e6e1fc007b480839d2f9f06bc6d31eb47f1ac3ea
h1tendra <hitendra.http@gmail.com>
2018-11-16 00:08:51 +0530

    get & debug git commit message for hockey app

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
-GIT_COMMIT_DESC=$1
+CIRCLE_SHA1=$1
+GIT_COMMIT_DESC=$(git log --format=oneline -n 1 $CIRCLE_SHA1)
+echo "printing git commit message"
+echo $GIT_COMMIT_DESC
 curl \
 -F "status=2" \
 -F "notify=2" \
--F "notes=$GIT_COMMIT_DESC" \
+-F "notes=NOTES:$GIT_COMMIT_DESC" \
 -F "notes_type=0" \
 -F "ipa=@platforms/android/app/build/outputs/apk/debug/app-debug.apk" \
 -H "X-HockeyAppToken: 2161769b31f54dc3a4dc9bef41b74300" \
